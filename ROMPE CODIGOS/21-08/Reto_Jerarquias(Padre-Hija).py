a98c18156786906b85a4db70ae4dfa98f8c9fc9c
Julian_github <worksedu0@gmail.com>
2026-08-26 01:19:58 -0500

    Update Reto_Jerarquias(Padre-Hija).py

--- ROMPE CODIGOS/21-08/Reto_Jerarquias(Padre-Hija).py
+++ ROMPE CODIGOS/21-08/Reto_Jerarquias(Padre-Hija).py
@@ -4,24 +4,29 @@ class Usuario:
         self.nombre = nombre  # Atributo del padre
         self.email = email    # Atributo del padre
 
-    # Método base para ser redefinido
+
+
+    # Método base para ser redefinido (Es una función NO Confundir con def __init__)
     def obtener_rol(self):
-        return "Usuario base del sistema"
-
-
+        return "Usuario creado en el sistema"  #Con esta funcion se crea el polimorfismo y asi se crea "las distintas carreras"
+    #POLIMORFISMO: MUCHAS COSAS
+    #El metodo devuelve unicamente una cadena de texto
+    
+    
 # Clase hija que hereda de Usuario
 class Estudiante(Usuario):
     def __init__(self, nombre, email, carrera):
         super().__init__(nombre, email)  # Reutiliza la inicialización del padre
         self.carrera = carrera          # Atributo propio del hijo
 
-    # Redefinición (override) del método del padre
+    # Redefinición o sobreescribe el método del padre usando, devolviendo o returnando la carrera
     def obtener_rol(self):
-        return "Estudiante de " + self.carrera
+        return f"Estudiante de {self.carrera}"
 
 
 # Ejecución directa del código
-estudiante = Estudiante("Sofía", "sofia@universidad.edu", "Ingeniería de Sistemas")
+estudiante = Estudiante("Sofía", "sofia@universidad.edu", "Ingeniería de Sistemas") #Se crea una estudiante como ejemplo
 
 print("Nombre:", estudiante.nombre)
 print("Rol:", estudiante.obtener_rol())
+print("Email:", estudiante.email)
