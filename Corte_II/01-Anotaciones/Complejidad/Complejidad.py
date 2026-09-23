07560f7196f86946a8d6af8c091acbba83233706
worksedu-prog <worksedu0@gmail.com>
2026-09-23 11:16:55 -0500

    Finalización de quizz

--- Corte_II/01-Anotaciones/Complejidad/Complejidad.py
+++ Corte_II/01-Anotaciones/Complejidad/Complejidad.py
@@ -5,4 +5,11 @@
 
 
 #Tipos de Big O:
-# O(1) - constante no cambia 
+
+# O(1) - constante no cambia con el tamaño de la entrada v[i]
+# O(log n) - logaritmica, divide el problema en partes iguales,busqueda binaria (sube uno o dos pasos mas)
+# O(n) - lineal, recorre todos los elementos de la entrada, busqueda secuencial(se dobla) recorrer lista enlazada
+# O(n log n) - logaritmica lineal, divide el problema en partes iguales y recorre todos los elementos de la entrada, mergesort, quicksort (un poco mas del doble)
+# O(n^2) - cuadratica, recorre todos los elementos de la entrada y para cada elemento recorre todos los elementos de la entrada, bubble sort, selection sort, insertion sort (cuadruplica)
+#Regla 1: se ignoran las constantes y los terminos de menor orden
+#regla 2: manda terminos quemás crece.
